5aea3b54c255814c159cf70eaa2ceda36c4e6c29
adit-bala <adityarbala@gmail.com>
2022-11-18 15:54:41 -0800

    refactor organization

--- start.py
+++ start.py
@@ -10,24 +10,29 @@ USER_AGENT = "adventofcode_working_directories_creator"
 URL = f"https://adventofcode.com/2022/day/{day}/input"
 cookie = os.getenv('COOKIE')
 
+# create folder for day
+if not os.path.exists(f"./{day}"):
+    os.makedirs(f"./{day}")
+
 # attempt to fetch inputs
-if not os.path.exists(f"./input/{day}.txt"):
+if not os.path.exists(f"./{day}/{day}.txt"):
     try:
         with requests.get(url=URL, cookies={"session": cookie}, headers={"User-Agent": USER_AGENT}) as response:
             if response.ok:
                 if response.ok:
                     data = response.text
-                    input = open(f"./input/{day}.txt", "w+")
+                    input = open(f"./{day}/{day}.txt", "w+")
                     input.write(data.rstrip("\n"))
                     input.close()
                 else:
                     print("Server response sus")
     except Exception as e:
         print("something went wrong lol")
+
 # try to set up code
-if not os.path.exists(f"./sol/{day}.py"):
-    code = open(f"./sol/{day}.py", "w+")
-    code.write(
-        "\n\nwith open((__file__.rstrip(\"code.py\")+\"input.txt\"), 'r') as input_file:\n    input = input_file.read()\n\n\n\nprint(\"Part One : \"+ str(None))\n\n\n\nprint(\"Part Two : \"+ str(None))")
+if not os.path.exists(f"./{day}/{day}.py"):
+    code = open(f"./{day}/{day}.py", "w+")
+    code.write(f"\n\nwith open((__file__.rstrip(\"{day}.py\")+\"{day}.txt\"), 'r') as input_file:\n input = input_file.read()\n\n\n\nprint(\"Part One : \"+ str(None))\n\n\n\nprint(\"Part Two : \"+ str(None))"
+    ) 
     code.close()
 
